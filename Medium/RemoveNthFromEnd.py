1bf7037beb399e82722e6dcfb44434e233e42693
Nolan Chappuis <nolanchappuis@hotmail.com>
2025-04-30 16:03:38 +0200

    Redid the same function to ensure I understood

--- Medium/RemoveNthFromEnd.py
+++ Medium/RemoveNthFromEnd.py
@@ -37,7 +37,49 @@ class Solution:
         dummy.next = dummy.next.next
 
         return res.next
+
+
+
+    def removeNthFromEndAgain(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        dummy.next = head
+        res = dummy
+
+        cur = head
+        count = 0 
+        while cur:
+            cur = cur.next
+            count += 1
+        n = count - n
+
+        if count == 1:
+            return None 
+
+        for _ in range(n):
+            dummy = dummy.next
+        if dummy.next and dummy.next.next:
+            dummy.next=dummy.next.next
+        else : dummy.next = None
+
+        return res.next
+
     
-print(Solution().removeNthFromEnd([1,2,3,4,5],2))
+    def printHelper(self, head: Optional[ListNode]):
+        while head:
+            print(head.val)
+            head = head.next
+
+
+l1 = ListNode(1)
+
+l1.next = ListNode(2)
+
+#l1.next.next = ListNode(3)
+
+#l1.next.next.next = ListNode(4)
+
+#l1.next.next.next.next = ListNode(5)
+
+Solution().printHelper(Solution().removeNthFromEndAgain(l1,2))
     
 
