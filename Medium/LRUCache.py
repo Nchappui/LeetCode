9fd59808e47676ef82fe55b2602c40820456d2fe
Nolan Chappuis <nolanchappuis@hotmail.com>
2025-05-01 11:11:05 +0200

    Changed the whole code from Linked List to Double Linked List i wanna die

--- Medium/LRUCache.py
+++ Medium/LRUCache.py
@@ -1,5 +1,5 @@
 class ListNode:
-    def __init__(self, val=0, prev = None, next=None):
+    def __init__(self, val=[], prev = None, next=None):
         self.val = val
         self.next = next
         self.prev = prev
@@ -7,7 +7,6 @@ class ListNode:
 
 class LRUCache:
 
-    #WORKS BUT SCALES POORLY
     def __init__(self, capacity: int):
         self.countSize = 0
         self.maxSize = capacity
@@ -16,66 +15,74 @@ class LRUCache:
         self.tail = self.head
 
     def get(self, key: int) -> int:
-        if hash(key) in self.hashMapValues:
+        if key in self.hashMapValues:
             self.updateValue(key)
-
-        print(self.hashMapValues.get(hash(key), -1))
-        return self.hashMapValues.get(hash(key), -1)
+            print(self.hashMapValues[key].val[1])
+            return self.hashMapValues[key].val[1]
+        
+        else:
+            print(-1)
+            return -1
 
     def put(self, key: int, value: int) -> None:
-        if hash(key) in self.hashMapValues:
+        if key in self.hashMapValues:
+            self.hashMapValues[key].val[1] = value
             self.updateValue(key)
-            self.hashMapValues[hash(key)] = value
+            #self.hashMapValues[key] = value
             return
         
         if self.countSize == 0:
-            self.head = self.tail = ListNode(hash(key), None)
-            self.hashMapValues[hash(key)] = value
+            self.head = self.tail = ListNode([key,value], None)
+            self.hashMapValues[key] = self.tail
             self.countSize += 1
 
         elif self.countSize < self.maxSize :
-            self.hashMapValues[hash(key)] = value
-            self.tail.next = ListNode(hash(key), None)
+            self.tail.next = ListNode([key,value], None)
             self.tail.next.prev = self.tail
             self.tail = self.tail.next
+            self.hashMapValues[key] = self.tail
             self.countSize += 1
             
         else:
-            self.hashMapValues[hash(key)] = value
-            self.tail.next = ListNode(hash(key), None)
+            self.tail.next = ListNode([key,value], None)
             self.tail.next.prev = self.tail
             self.tail = self.tail.next
-            self.hashMapValues.pop(self.head.val)
+            self.hashMapValues.pop(self.head.val[0])
             self.head = self.head.next
+            self.head.prev = None
+            self.hashMapValues[key] = self.tail
 
 
     def updateValue(self, key):
         ## One element or last element -> do nothing
-        if self.countSize == 1 or self.tail.val == hash(key):
+        if self.countSize == 1 or self.tail == self.hashMapValues[key]:
             pass
         ## Element is the head -> shift it to last place
-        elif self.head.val == hash(key):
+        elif self.head.val[0] == key:
+            value = self.head.val[1]
             self.head = self.head.next
-            self.tail.next = ListNode(hash(key), None)
+            self.head.prev = None
+            self.tail.next = ListNode([key,value], None)
             self.tail.next.prev = self.tail
             self.tail = self.tail.next
+            self.hashMapValues[key] = self.tail
         ## Element is between the head and the tail -> shift it to last place
         else:
-            cur = self.head
-            while cur.next:
-                if cur.next.val == hash(key):
-                    cur.next = cur.next.next
-                cur = cur.next
-            self.tail.next = ListNode(hash(key), None)
+            elem = self.hashMapValues[key]
+            value = elem.val[1]
+            elem.prev.next = elem.next
+            elem.next.prev = elem.prev
+            self.tail.next = ListNode([key,value], None)
             self.tail.next.prev = self.tail
             self.tail = self.tail.next
+            self.hashMapValues[key] = self.tail
 
-
+'''
 lRUCache = LRUCache(3)
 lRUCache.put(1, 1)
 lRUCache.put(2, 2)
 lRUCache.put(3, 3)
-lRUCache.put(4, 4)
+lRUCache.put(4, 5)
 lRUCache.get(4)
 lRUCache.get(3)
 lRUCache.get(2) 
@@ -86,3 +93,12 @@ lRUCache.get(2)
 lRUCache.get(3)
 lRUCache.get(4)
 lRUCache.get(5)
+'''
+
+lRUCache = LRUCache(2)
+lRUCache.put(2, 1)
+lRUCache.put(2, 2)
+lRUCache.get(2)
+lRUCache.put(1, 1)
+lRUCache.put(4, 1)
+lRUCache.get(2)
