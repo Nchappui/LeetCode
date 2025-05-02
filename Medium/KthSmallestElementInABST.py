50c3e97d854bbc963bb7cb5bc5142e20cd9d467d
Nolan Chappuis <nolanchappuis@hotmail.com>
2025-05-02 13:59:00 +0200

    Add implementation for finding the Kth smallest element in a BST

---
+++ Medium/KthSmallestElementInABST.py
@@ -0,0 +1,42 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        result = None
+        def inOrder(root):
+            nonlocal k
+            nonlocal result
+            if not root or result:
+                return
+            inOrder(root.left)
+            if k == 1:
+                k = k - 1
+                result = root.val
+                return
+            k = k - 1
+            inOrder(root.right)
+        inOrder(root)
+        return result
+        
+
+    '''
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        stack = []
+        def dfs(root):
+            if not root:
+                return
+            stack.append(root.val)
+            dfs(root.left)
+            dfs(root.right)
+        dfs(root)
+        stack.sort()
+        return stack[k-1]
+        '''
