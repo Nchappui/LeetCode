0cd67b4f9e20da74d19d09d2005ee094209c30eb
Nolan Chappuis <nolanchappuis@hotmail.com>
2025-05-08 11:03:38 +0200

    Implement working solution for Word Search II using Trie with DFS

--- Hard/WordSearch2.py
+++ Hard/WordSearch2.py
@@ -37,8 +37,51 @@ class SolutionFalse:
                 res.append(word)
             
         return res
-    
 
+##WORKING SOLUTION WITH TRIE CONSTRUCTED ON WORDS  
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        # Construire le Trie à partir des mots
+        def buildTrie(words):
+            trie = {}
+            for word in words:
+                node = trie
+                for letter in word:
+                    if letter not in node:
+                        node[letter] = {}
+                    node = node[letter]
+                node["#"] = True  # Marque la fin d'un mot
+            return trie
+
+        # Recherche DFS dans le plateau
+        def dfs(x, y, node, path):
+            letter = board[x][y]
+            if letter not in node:
+                return
+            node = node[letter]
+            path += letter
+            if "#" in node:  # Si un mot est trouvé
+                result.add(path)
+
+            # Marquer la cellule comme visitée
+            temp, board[x][y] = board[x][y], "@"
+            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < len(board) and 0 <= ny < len(board[0]) and board[nx][ny] != "@":
+                    dfs(nx, ny, node, path)
+            # Restaurer la cellule
+            board[x][y] = temp
+
+        # Étape 1 : Construire le Trie
+        trie = buildTrie(words)
+
+        # Étape 2 : Parcourir le plateau et rechercher les mots
+        result = set()
+        for x in range(len(board)):
+            for y in range(len(board[0])):
+                dfs(x, y, trie, "")
+
+        return list(result)
 
 print(Solution().findWords([["c","d"],
                             ["b","a"]],
