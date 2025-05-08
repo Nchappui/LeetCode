3ca6d0296513eda6bfd12fb47ac7745a8ad94193
Nolan Chappuis <nolanchappuis@hotmail.com>
2025-05-08 14:06:41 +0200

    Implement permutation function in Solution class

--- Medium/Permutations.py
+++ Medium/Permutations.py
@@ -3,4 +3,21 @@ from typing import List
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        NotImplemented
+        answer = []
+        def recFunc(curr):
+            validNumbers = nums.copy()
+            if len(curr) == len(nums):
+                answer.append(curr.copy())
+                return
+            for num in curr:
+                validNumbers.remove(num)
+            for num in validNumbers:
+                curr.append(num)
+                recFunc(curr)
+                curr.pop()
+        recFunc([])
+        return answer
+    
+print(Solution().permute([1,2,3]))
+print(Solution().permute([0,1]))
+print(Solution().permute([1]))
