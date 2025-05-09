130aed34612249d0464a508b8d4a82474b2e0e2d
Nolan Chappuis <nolanchappuis@hotmail.com>
2025-05-09 11:08:58 +0200

    Add implementation of maximum subarray algorithm

---
+++ Medium/MaximumSubarray.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:            
+        res = nums[0]
+        total = 0
+
+        for n in nums:
+            if total < 0:
+                total = 0
+
+            total += n
+            res = max(res, total)
+        
+        return res
+            
+Solution().maxSubArray([-4,-3,-1,-10])
