aa6d4c79910223bd42ecce726708e48bbb119cd3
Nolan Chappuis <nolanchappuis@hotmail.com>
2025-05-09 10:42:00 +0200

    Refactor mergeKLists method to streamline empty list handling and improve readability

--- Hard/MergeKSortedLists.py
+++ Hard/MergeKSortedLists.py
@@ -8,25 +8,48 @@ class ListNode:
         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        dummy = ListNode()
-        cur = dummy
-
-        smallestValue = float('inf')
+        res = ListNode()
+        dummy = res
         smallestNode = ListNode()
-        while(lists):
-            for list in lists:
-                if list.val < smallestValue:
-                    smallestValue = list.val
-                    smallestNode = list
 
-            cur.next = smallestNode
-            for list in lists:
-                if list == smallestNode:
-                    list=list.next
-                if list : 
-                    continue
-                else:
-                    lists.remove(list)
-        return cur.next
+        #Remove empty lists
+        noneCounter = 0
+        for head in lists:
+            if not head:
+                noneCounter += 1
+        while noneCounter > 0:
+            lists.remove(None)
+            noneCounter -= 1
+
+        smallestPointer = 0
+        while(lists):
+            count = 0
+            smallestValue = float('inf')
+            for head in lists:
+                if head.val < smallestValue:
+                    smallestValue = head.val
+                    smallestNode = head
+                    smallestPointer = count
+                count += 1
+            dummy.next = smallestNode
+            dummy = dummy.next
+            lists[smallestPointer] = lists[smallestPointer].next
+            if not lists[smallestPointer]:
+                lists.remove(None)
+
+        return res.next
             
         
+a = ListNode(1)
+a.next = ListNode(4)
+a.next.next = ListNode(5)
+
+b = ListNode(1)
+b.next = ListNode(3)
+b.next.next = ListNode(4)
+
+c = ListNode(2)
+c.next = ListNode(6)
+
+Solution().mergeKLists([a,b,c])
+Solution().mergeKLists([None, None])
