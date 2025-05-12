9801dfb0a984cade9af7cae56ef2619a79d082ef
Nolan Chappuis <nolanchappuis@hotmail.com>
2025-05-12 15:07:10 +0200

    Add implementation of trailingZeroes method to count trailing zeroes in factorial of a number

---
+++ Medium/FactorialTrailingzeroes.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def trailingZeroes(self, n):
+        count = 0
+        divisor = 5
+
+        while n >= divisor:
+            count += n // divisor
+            divisor *= 5
+
+        return count
+        
+
+Solution().trailingZeroes(5)
