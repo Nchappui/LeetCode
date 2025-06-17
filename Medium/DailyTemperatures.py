0cb148f50e8e4a192a1fb9dcc61ca4d0e75ab309
Nolan Chappuis <nolanchappuis@hotmail.com>
2025-06-17 15:01:52 +0200

    Implement dailyTemperatures method to calculate the number of days until a warmer temperature

--- Medium/DailyTemperatures.py
+++ Medium/DailyTemperatures.py
@@ -3,5 +3,14 @@ from typing import List
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        
-        return temperatures
+        n = len(temperatures)
+        stack = [0]
+        res = [0] * n
+        for i in range(1, n):
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                id = stack.pop()
+                res[id] = i - id
+            stack.append(i)
+        return res
+    
+Solution().dailyTemperatures([73,74,75,71,69,72,76,73])
