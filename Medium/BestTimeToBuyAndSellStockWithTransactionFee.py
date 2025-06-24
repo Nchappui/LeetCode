3501b73fbc744a9571f3d181bc4aa2afed9b672f
Nolan Chappuis <nolanchappuis@hotmail.com>
2025-06-24 15:27:58 +0200

    Implement maxProfit method to calculate maximum profit from stock prices with transaction fee

---
+++ Medium/BestTimeToBuyAndSellStockWithTransactionFee.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution(object):
+    def maxProfit(self, prices, fee):
+        hold = -prices[0]
+        cash = 0
+
+        for price in prices[1:]:
+            cash = max(cash, hold + price - fee)
+            hold = max(hold, cash - price)
+
+        return cash
+                
+
+Solution().maxProfit([1,5,9],2)
